a0c3acf27d7bbd50079c13f2f6d9efb48c7b7473
Adam Varsányi <xvarsa01@stud.fit.vut.cz>
2024-10-20 18:38:23 +0200

    feature: DAL repos

---
+++ Simt.DAL/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Simt.DAL.entities;
+
+namespace Simt.DAL.Repositories;
+
+public interface IRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    IQueryable<TEntity> Get();
+    Task DeleteAsync(Guid entityId);
+    ValueTask<bool> ExistsAsync(TEntity entity);
+    Task<TEntity> InsertAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+}
---
+++ Simt.DAL/Repositories/Repositry.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Simt.DAL.entities;
+using Simt.DAL.Mappers;
+
+namespace Simt.DAL.Repositories;
+
+public class RepositoryVehicle<TEntity>(DbContext dbContext, IEntityMapper<TEntity> entityMapper) : IRepository<TEntity> 
+    where TEntity : class, IEntity
+{
+    private readonly DbSet<TEntity> _dbSet = dbContext.Set<TEntity>();
+    private readonly IEntityMapper<TEntity> _entityMapper = entityMapper;
+    
+    public IQueryable<TEntity> Get()
+    {
+        return _dbSet;
+    }
+
+    public async Task DeleteAsync(Guid entityId)
+    {
+        _dbSet.Remove(await _dbSet.SingleAsync(i => i.Id == entityId).ConfigureAwait(false));
+    }
+
+    public async ValueTask<bool> ExistsAsync(TEntity entity)
+    {
+        return entity.Id != Guid.Empty && await _dbSet.AnyAsync(e => e.Id == entity.Id);
+    }
+
+    public async Task<TEntity> InsertAsync (TEntity entity)
+    {
+        return (await _dbSet.AddAsync(entity)).Entity;
+    }
+
+    public async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        TEntity existingEntity = await _dbSet.SingleAsync(e => e.Id == entity.Id);
+        _entityMapper.MapToExistingEntity(existingEntity, entity);
+        return existingEntity;
+    }
+}
