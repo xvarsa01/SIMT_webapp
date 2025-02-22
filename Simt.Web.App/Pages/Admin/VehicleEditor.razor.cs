c67fd6a28be4c3f9c21389118c87c9f1cbddc8e7
xvarsa01 <xvarsa01@vutbr.cz>
2025-02-22 21:32:35 +0100

    feature: sorting and filtering vehicles in VehicleEditor

--- Simt.Web.App/Pages/Admin/VehicleEditor.razor.cs
+++ Simt.Web.App/Pages/Admin/VehicleEditor.razor.cs
@@ -9,18 +9,143 @@ public partial class VehicleEditor : ComponentBase
     [Inject]
     public VehicleFacade VehicleFacade { get; set; } = null!;
 
-    private List<VehicleListModel> VehicleList { get; set; } = new();
+    private List<VehicleListModel> VehicleListActual { get; set; } = new();
+    private List<VehicleListModel> VehicleListFirstLoad { get; set; } = new();
     private List<string> AllManufacturers { get; set; } = new();
     private List<string> AllTypes { get; set; } = new();
     private List<string> AllOperators { get; set; } = new();
     
-    private string selectedManufacturer = "";
-    private string selectedType = "";
-    private string selectedOperator = "";
+    private string _selectedManufacturer = "";
+    private string _selectedType = "";
+    private string _selectedOperator = "";
+    private string _selectedVehicleNumber = "";
     
     protected override async Task OnInitializedAsync()
     {
-        VehicleList = await VehicleFacade.GetAllAsync();
+        VehicleListFirstLoad = VehicleListActual = (await VehicleFacade.GetAllAsync())
+            .OrderBy(x => x.Status)
+            .ThenBy(x => x.VehicleNumber)
+            .ToList();
+        AllManufacturers = VehicleListFirstLoad.Select(x => x.Manufacturer).Distinct().ToList();
+        AllTypes = VehicleListFirstLoad.Select(x => x.Type).Distinct().ToList();
+        AllOperators = VehicleListFirstLoad.Select(x => x.Operator).Distinct().ToList();
+    }
+    
+    private void OnSearchVehicleByManufacturer(string value)
+    {
+        if (!string.IsNullOrEmpty(value))
+        {
+            _selectedManufacturer = value;
+            RecalculateList();
+        }
+        else
+        {
+            _selectedManufacturer = "";
+            RecalculateList();
+            AllManufacturers = VehicleListFirstLoad.Select(x => x.Manufacturer).Distinct().ToList();
+        }
+        if (_selectedType == "")
+        {
+            AllTypes = VehicleListActual.Select(x => x.Type).Distinct().ToList();
+        }
+        if (_selectedOperator == "")
+        {
+            AllOperators = VehicleListActual.Select(x => x.Operator).Distinct().ToList();
+        }
+    }
+    private void OnSearchVehicleByType(string value)
+    {
+        if (!string.IsNullOrEmpty(value))
+        {
+            _selectedType = value;
+            RecalculateList();
+        }
+        else
+        {
+            _selectedType = "";
+            RecalculateList();
+            AllTypes = VehicleListActual.Select(x => x.Type).Distinct().ToList();
+        }
+
+        if (_selectedManufacturer == "")
+        {
+            AllManufacturers = VehicleListActual.Select(x => x.Manufacturer).Distinct().ToList();
+        }
+        if (_selectedOperator == "")
+        {
+            AllOperators = VehicleListActual.Select(x => x.Operator).Distinct().ToList();
+        }
+    }
+    private void OnSearchVehicleByOperator(string value)
+    {
+        if (!string.IsNullOrEmpty(value))
+        {
+            _selectedOperator = value;
+            RecalculateList();
+        }
+        else
+        {
+            _selectedOperator = "";
+            RecalculateList();
+            AllOperators = VehicleListActual.Select(x => x.Operator).Distinct().ToList();
+        }
+        if (_selectedManufacturer == "")
+        {
+            AllManufacturers = VehicleListActual.Select(x => x.Manufacturer).Distinct().ToList();
+        }
+        if (_selectedType == "")
+        {
+            AllTypes = VehicleListActual.Select(x => x.Type).Distinct().ToList();
+        }
+    }
+    private void OnSearchVehicleNumberChange(ChangeEventArgs args)
+    {
+        var value = args.Value?.ToString();
+        if (!string.IsNullOrEmpty(value))
+        {
+            _selectedVehicleNumber = value;
+        }
+        else
+        {
+            _selectedVehicleNumber = "";
+        }
+        RecalculateList();
+        if (_selectedManufacturer == "")
+        {
+            AllManufacturers = VehicleListActual.Select(x => x.Manufacturer).Distinct().ToList();
+        }
+        if (_selectedType == "")
+        {
+            AllTypes = VehicleListActual.Select(x => x.Type).Distinct().ToList();
+        }
+        if (_selectedOperator == "")
+        {
+            AllOperators = VehicleListActual.Select(x => x.Operator).Distinct().ToList();
+        }
+    }
+
+    private void RecalculateList()
+    {
+        VehicleListActual = VehicleListFirstLoad;
+
+            Console.WriteLine(_selectedManufacturer + " " + _selectedType + " " + _selectedOperator + " " + _selectedVehicleNumber);
+        if (!string.IsNullOrEmpty(_selectedManufacturer))
+        {
+            VehicleListActual = VehicleListActual.Where(v => v.Manufacturer == _selectedManufacturer).ToList();
+        }
+        if (!string.IsNullOrEmpty(_selectedType))
+        {
+            VehicleListActual = VehicleListActual.Where(v => v.Type == _selectedType).ToList();
+        }
+        if (!string.IsNullOrEmpty(_selectedOperator))
+        {
+            VehicleListActual = VehicleListActual.Where(v => v.Operator == _selectedOperator).ToList();
+        }
+        if (!string.IsNullOrEmpty(_selectedVehicleNumber))
+        {
+            VehicleListActual = VehicleListActual.Where(v =>
+                v.VehicleNumber.Contains(_selectedVehicleNumber, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
     }
     
     private void CreateNewVehicle()
