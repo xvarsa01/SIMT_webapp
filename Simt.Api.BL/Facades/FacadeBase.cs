53832c29a9066aa91375f12c44b63b6f56eed28f
xvarsa01 <xvarsa01@vutbr.cz>
2025-05-03 00:00:34 +0200

    feature: Deleted method in FacadeBase.cs put into try-catch

--- Simt.Api.BL/Facades/FacadeBase.cs
+++ Simt.Api.BL/Facades/FacadeBase.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using Simt.Api.BL.Facades.Interface;
 using Simt.Api.BL.Mappers.InterfaceBase;
 using Simt.Api.DAL.entities;
@@ -69,7 +70,14 @@ public class FacadeBase<TRepository, TEntity, TListModel, TDetailModel, TCreatio
     
     public virtual async Task DeleteAsync(Guid id)
     {
-        await Repository.DeleteAsync(id);
+        try
+        {
+            await Repository.DeleteAsync(id);
+        }
+        catch (DbUpdateException e)
+        {
+            throw new InvalidOperationException("Entity deletion failed.", e);
+        }
     }
     
     public static void GuardCollectionsAreNotSet(TCreationModel model)
